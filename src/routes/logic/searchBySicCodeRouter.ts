5179e26f9ec994e7c22a7b6f58445360dc67823f
IqraL <IqraLatif159@gmail.com>
2024-07-12 12:43:38 +0100

    adding sorting

--- src/routes/logic/searchBySicCodeRouter.ts
+++ src/routes/logic/searchBySicCodeRouter.ts
@@ -5,29 +5,30 @@ export const filterSecCodes = async (
   req: FilterSecCodesRequest,
   res: Response
 ) => {
-   try {
-     const client = await MongoDbClient.getClient();
-     const cursor = await client
-       .db("companies-db")
-       .collection("information")
-       .find({ sic_codes: { $in: req.body.sic_codes } });
+  try {
+    const client = await MongoDbClient.getClient();
+    const cursor = await client
+      .db("companies-db")
+      .collection("information")
+      .find({ sic_codes: { $in: req.body.sic_codes } })
+      .sort({ companyName: 1, _id: 1 });
 
-     res.setHeader("Content-Type", "application/json");
-     res.write("["); // Start the JSON array
+    res.setHeader("Content-Type", "application/json");
+    res.write("["); // Start the JSON array
 
-     let first = true;
-     for await (const doc of cursor) {
-       if (!first) {
-         res.write(","); // Add a comma between documents
-       }
-       first = false;
-       res.write(JSON.stringify(doc)); // Write the document as a JSON string
-     }
+    let first = true;
+    for await (const doc of cursor) {
+      if (!first) {
+        res.write(","); // Add a comma between documents
+      }
+      first = false;
+      res.write(JSON.stringify(doc)); // Write the document as a JSON string
+    }
 
-     res.write("]"); // End the JSON array
-     res.end();
-   } catch (error) {
-     console.error("Error fetching data:", error);
-     res.status(500).json({ error: "Internal Server Error" });
-   }
+    res.write("]"); // End the JSON array
+    res.end();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
--- src/routes/types/index.ts
+++ src/routes/types/index.ts
@@ -1,11 +1,17 @@
 import { Request, Response } from "express";
 
 export { Response };
-
+type SortType =
+  | "companyName"
+  | "companyNumber"
+  | "CompanyStatus"
+  | "NumberOfEmployees"
+  | "Industry"
+  | "";
 export type FilterSecCodesRequest = Request<
   {},
   {},
-  { sic_codes: string[] },
+  { sic_codes: string[]; sort: SortType },
   {},
   {}
 >;
