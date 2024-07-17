8362794756fa97787d407c45807398082ee20fd0
IqraL <IqraLatif159@gmail.com>
2024-07-17 21:40:31 +0100

    dotenv change

--- src/secrets/index.ts
+++ src/secrets/index.ts
@@ -1,6 +1,6 @@
-import "dotenv/config";
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 
 const MONGO_DB_URI = `mongodb+srv://iqra:${process.env.db_password}@companies-list-severles.fze0gqd.mongodb.net/?retryWrites=true&w=majority&appName=companies-list-severless-db`;
-
+console.log("db_password", process.env.db_password);
 export { MONGO_DB_URI };
